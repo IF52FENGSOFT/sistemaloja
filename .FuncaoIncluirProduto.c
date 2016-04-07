d932892b8aa6826d9953ce77eb9bee5ccf3a7c30
IF52FENGSOFT <sirnande.santo@hotmail.com>
2016-04-07 13:41:58 -0300

    Update .FuncaoIncluirProduto.c

--- .FuncaoIncluirProduto.c
+++ .FuncaoIncluirProduto.c
@@ -1,7 +1,53 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "myproduto.h"
 
-int Incluir(){
-  printf("Esse funcao inclui: ");
-  
-  return 0;
+void CadastroProduto(cadproduto vetor[], int car[], int num){
+    int veri = 0;
+    printf("===================== CADASTRO PRODUTO =====================\n");
+    printf("CODIGO PRODUTO: ");
+    scanf("%d",&vetor[num].codigo);
+    do{
+        veri = VerificarCodigo(vetor, num, vetor[num].codigo);
+        if(veri == 0){
+            printf("!!! ERRO ESSE CODIGO JA EXISTE DIGITE UM CODIGO DIFERENTE DE [ %d ] !!!\n"  ,vetor[num].codigo);
+            scanf("%d",&vetor[num].codigo);
+        };
+    }while(veri == 0);
+
+    printf("NOME: ");
+    fflush(stdin);
+    scanf("%[^\n]s",&vetor[num].nome);
+    printf("DESCICAO: ");
+    fflush(stdin);
+    scanf("%[^\n]s",&vetor[num].descricao);
+    printf("QUANTIDADE: ");
+    scanf("%d",&vetor[num].quantidade);
+    printf("VALOR: ");
+    scanf("%f",&vetor[num].precovenda);
+    printf("MODELO: ");
+    fflush(stdin);
+    scanf("%[^\n]s",&vetor[num].modelo);
+    printf("COR: ");
+    fflush(stdin);
+    scanf("%[^\n]s",&vetor[num].cor);
+    printf("FABRICANTE: ");
+    fflush(stdin);
+    scanf("%[^\n]s",&vetor[num].fabricante);
+    printf("CATEGORIA: ");
+    fflush(stdin);
+    scanf("%[^\n]s",&vetor[num].categoria);
+}
+
+int VerificarCodigo(cadproduto vetor[], int num, int numero){
+    int i ;;
+    for(i = 0; i < num; i++){
+        if(vetor[i].codigo == numero){
+
+            return 0;
+        }
+    }
+
+    return 1;
 }
