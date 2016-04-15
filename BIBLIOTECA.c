98d6da6d56e5ca8bceec42d9c43b4ae05b5cba3b
IF52FENGSOFT <sirnande.santo@hotmail.com>
2016-04-15 15:08:28 -0300

    Create BIBLIOTECA.c

---
+++ BIBLIOTECA.c
@@ -0,0 +1,16 @@
+#ifndef MYPRODUTO_H
+#define MYPRODUTO_H
+
+struct produto{
+
+  int codigo, quantidade;
+  char descricao[30], modelo[10], cor[10], fabricante[30], categoria[40], nome[30];
+  double precovenda;
+
+}typedef cadproduto;
+
+
+
+
+#endif // MYPRODUTO_H
+
